8ec9d12aaf9c0c38b527cb9aa704826361c57673
bruce.lu <lzbgt@126.com>
2014-01-05 05:27:23 +0800

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,5 +3,15 @@ class PostsController < ApplicationController
     def new
     end
 
+    def create
+      @post = Post.new(post_params)
+      @post.save
+      redirect_to @post
+    end
+
+    private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end
 
 end
