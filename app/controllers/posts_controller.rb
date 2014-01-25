aa3c6072f998dea293d93aa0992959f6f7bb2475
bruce.lu <lzbgt@126.com>
2014-01-25 16:42:08 +0800

    v2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,47 @@
 class PostsController < ApplicationController
 
-    def new
-      @post = Post.new
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
-    def create
-      @post = Post.new(post_params)
+  def show
+    @post = Post.find(params[:id])
 
-      if @post.save
-        redirect_to @post
-      else
-        render 'new'
-      end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  public
+  def edit
+    @post = Post.find(params[:id])
+    puts "====================Edit post: #{@post.inspect}"
+
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
     end
-
-    def show
-      @post = Post.find(params[:id])
-    end
-
-    def index
-      @posts = Post.all
-    end
-
-    private
-    def post_params
-      params.require(:post).permit(:title, :text)
-    end
-
+  end
 end
