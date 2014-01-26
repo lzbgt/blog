2ccbb433db9dc80dc947d9dbff3735d26a7e6c1d
bruce.lu <lzbgt@126.com>
2014-01-26 12:04:32 +0800

    v2.2 comment model added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -44,4 +44,12 @@ class PostsController < ApplicationController
       render 'edit'
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20140126035854) do
 
-ActiveRecord::Schema.define(version: 20140104212050) do
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+
   create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
 
 end
